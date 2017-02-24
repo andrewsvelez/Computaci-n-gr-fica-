9c5cdaa63af7d63567193d4c8cc340d1de472c79
andrewsvelez <edanvelez@utp.edu.co>
2017-02-24 11:34:56 -0500

    calculando operacion trigonometrica

---
+++ trigo.py
@@ -0,0 +1,10 @@
+import math
+
+if __name__ == '__main__':
+    valor=60
+    print math.sin(60)
+    an=math.radians(90)
+    x=10
+    y=20
+    xp=(x*math.cos(an))+(y*math.sin(an))
+    print 'xp:',xp
